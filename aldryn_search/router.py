a58a8b971c4dbaf800556755f41c9266fb1ede2a
Felipe Prenholato <philipe.rp@gmail.com>
2015-04-16 11:14:33 -0300

    Switch Django get_language for CMS get_current_language to avoid Django issue https://code.djangoproject.com/ticket/9340

--- aldryn_search/router.py
+++ aldryn_search/router.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.utils.translation import get_language
+from cms.utils.i18n import get_current_language
 
 from haystack import routers
 from haystack.constants import DEFAULT_ALIAS
@@ -9,13 +9,13 @@ from haystack.constants import DEFAULT_ALIAS
 class LanguageRouter(routers.BaseRouter):
 
     def for_read(self, **hints):
-        language = get_language()
+        language = get_current_language()
         if language not in settings.HAYSTACK_CONNECTIONS:
             return DEFAULT_ALIAS
         return language
 
     def for_write(self, **hints):
-        language = get_language()
+        language = get_current_language()
         if language not in settings.HAYSTACK_CONNECTIONS:
             return DEFAULT_ALIAS
         return language
