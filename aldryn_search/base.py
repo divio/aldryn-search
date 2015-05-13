690f44ee055d5736b164229255a28d11e50b96d1
Felipe Prenholato <philipe.rp@gmail.com>
2015-05-13 00:13:06 -0300

    Add a method to check if object being indexed has app_config and namespace in public pages.

--- aldryn_search/base.py
+++ aldryn_search/base.py
@@ -3,10 +3,18 @@ import warnings
 from django.utils.translation import override
 
 from haystack import indexes
+from haystack.exceptions import DoNotIndex
 
 from .conf import settings
 from .helpers import get_request
 from .utils import clean_join, _get_language_from_alias_func
+
+try:
+    from aldryn_apphooks_config.utils import get_apphook_field_names
+    from cms.models import Page
+    HAS_APPCONFIG = True
+except ImportError:
+    HAS_APPCONFIG = False
 
 
 language_from_alias = _get_language_from_alias_func()
@@ -130,7 +138,30 @@ class AldrynIndexBase(AbstractIndex):
         """
         return None
 
+    def check_for_app_config(self, obj):
+        """
+        This should check for app_config in object and if it exists check
+        if app_config namespace exists in some page. If app_config exists
+        and namespace does not exists in pages we should skip indexing for
+        this object. If app_config does not exists for given object or we
+        can't import appconfig stuff, just ignore.
+        """
+        if HAS_APPCONFIG:
+            apphook_field_names = get_apphook_field_names(obj)
+            results = []
+
+            for field_name in apphook_field_names:
+                app_config = getattr(obj, field_name)
+                if app_config:
+                    pages = Page.objects.public().filter(
+                        application_namespace=app_config.namespace
+                    )
+                    results.append(pages.exists())
+            if not any(results):
+                raise DoNotIndex
+
     def prepare_fields(self, obj, language, request):
+        self.check_for_app_config(obj)
         self.prepared_data['language'] = language
         # We set the following fields here because on some models,
         # the value of these fields is dependent on the active language
--- setup.py
+++ setup.py
@@ -2,36 +2,43 @@ import metadata as m
 
 from setuptools import setup, find_packages
 
-install_requires = [
+INSTALL_REQUIRES = [
     'lxml',
     'setuptools',
-    'Django>=1.4',
+    'Django>=1.4,<1.7',
     'django-appconf',
     'django-cms>=2.4',
     'django-classy-tags>=0.3.2',
-    'django-haystack>=2.0.0',
+    'django-haystack>=2.4.0.dev0',
     'django-spurl',
     'django-standard-form',
     'aldryn-common',
 ]
 
+DEPENDENCY_LINKS = [
+    'https://github.com/chronossc/django-haystack/archive/feature/do_not_index.zip#egg=django-haystack-2.4.0.dev0'  # NOQA
+]
+
 setup(
-    name = m.name,
-    version = m.version,
-    url = m.project_url,
-    license = m.license,
+    name=m.name,
+    version=m.version,
+    url=m.project_url,
+    license=m.license,
     platforms=['OS Independent'],
-    description = m.description,
-    author = m.author,
-    author_email = m.author_email,
+    description=m.description,
+    author=m.author,
+    author_email=m.author_email,
     packages=find_packages(),
-    install_requires = install_requires,
-    include_package_data = True, #Accept all data files and directories matched by MANIFEST.in or found in source control.
-    package_dir = {
-        m.package_name:m.package_name,
+    install_requires=INSTALL_REQUIRES,
+    dependency_links=DEPENDENCY_LINKS,
+    # Accept all data files and directories matched by MANIFEST.in or
+    # found in source control.workon
+    include_package_data=True,
+    package_dir={
+        m.package_name: m.package_name,
     },
     zip_safe=False,
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
