8cfcedb3b1d9c1c4b1c1e60432cfc09381e326da
marco federighi <federighi.marco@gmail.com>
2015-07-07 16:26:39 +0200

    Add HAYSTACK CONNECTIONS

--- test_settings.py
+++ test_settings.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+HAYSTACK_CONNECTIONS = {
+'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://localhost:9001/solr/default',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+        'EXCLUDED_INDEXES': ['thirdpartyapp.search_indexes.BarIndex'],
+    },
+    'en': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://my-solr-server/solr/my-site-en/',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+    },
+}
 
 HELPER_SETTINGS = {
     'ALLOWED_HOSTS': ['localhost'],
     'CMS_LANGUAGES': {1: [{'code': 'en', 'name': 'English'}]},
-    'CMS_TEMPLATES': (("whee.html", "Whee Template"),),
     'LANGUAGES': (('en', 'English'),),
     'LANGUAGE_CODE': 'en',
     'TEMPLATE_LOADERS': ('aldryn_search.tests.FakeTemplateLoader',),
+    'HAYSTACK_CONNECTIONS': HAYSTACK_CONNECTIONS,
 }
 
 
