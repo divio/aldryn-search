43f384c6482c60664d388c92613ac3b37025466d
Paulo <commonzenpython@gmail.com>
2015-08-31 17:31:52 -0400

    be consistent with other aldryn apps

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import shlex
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
