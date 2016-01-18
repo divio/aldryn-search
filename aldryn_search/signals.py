c579a07122ef5d8931a58d4046f31c9a94b3d536
jakob <jakob.oswald@gmx.net>
2016-01-18 15:57:57 +0100

    Renamed cms_action -> object_action

--- aldryn_search/signals.py
+++ aldryn_search/signals.py
@@ -6,5 +6,5 @@ Created on Nov 30, 2015
 '''
 from django.dispatch.dispatcher import Signal
 
-add_to_index = Signal(providing_args=['instance', 'cms_action'])
-remove_from_index = Signal(providing_args=['instance', 'cms_action'])
+add_to_index = Signal(providing_args=['instance', 'object_action'])
+remove_from_index = Signal(providing_args=['instance', 'object_action'])
