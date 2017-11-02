44edf0ef717017b09a71c7332fdb8f6cc19ce556
Stefan Foulis <stefan@foulis.ch>
2017-11-02 09:42:38 +0100

    Update metadata for pypi

--- metadata.py
+++ metadata.py
@@ -4,5 +4,5 @@ author = 'Benjamin Wohlwend'
 author_email = 'piquadrat@gmail.com'
 description = "An extension to django CMS to provide multilingual Haystack indexes"
 version = __import__(package_name).__version__
-project_url = 'http://github.com/piquadrat/%s' % name
+project_url = 'http://github.com/aldryn/%s' % name
 license = 'BSD'
