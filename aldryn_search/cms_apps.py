0b676b9c022e982b8974e347f1c6c06cb23d40d2
kynsi <zingendevogel@gmail.com>
2018-03-09 18:01:19 +0100

    djan go-cms 3.5

--- aldryn_search/cms_apps.py
+++ aldryn_search/cms_apps.py
@@ -2,14 +2,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
 
 from .conf import settings
 
 
+@apphook_pool.register
 class AldrynSearchApphook(CMSApp):
     name = _("aldryn search")
-    urls = ['aldryn_search.urls']
-
-
-if settings.ALDRYN_SEARCH_REGISTER_APPHOOK:
-    apphook_pool.register(AldrynSearchApphook)
+    
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ["aldryn_search.urls"]  
