3da24538fb6a6535974fe5a2fbd4110e4ef59261
kynsi <zingendevogel@gmail.com>
2018-03-09 18:29:12 +0100

    Update cms_apps.py

--- aldryn_search/cms_apps.py
+++ aldryn_search/cms_apps.py
@@ -11,7 +11,7 @@ from .conf import settings
 @apphook_pool.register
 class AldrynSearchApphook(CMSApp):
     name = _("aldryn search")
-    app_name = 'aldryn_search'
+    app_name = 'aldryn-search'
     
     def get_urls(self, page=None, language=None, **kwargs):
         return ['aldryn_search.urls']  
