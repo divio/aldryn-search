92b332763093d6f68f07c00e1e32862e40ce7c03
carderm <michael.carder@michaelcarder.co.uk>
2018-04-27 16:22:53 +1000

    Remove whitespace for blanklines...

--- aldryn_search/base.py
+++ aldryn_search/base.py
@@ -14,7 +14,7 @@ language_from_alias = _get_language_from_alias_func()
 
 class AbstractIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=False)
-    
+
     def _get_backend(self, using):
         return self.get_backend(using)
 
@@ -26,7 +26,7 @@ class AbstractIndex(indexes.SearchIndex):
             self._backend = super(AbstractIndex, self).get_backend(using)
         else:
             self._backend = super(AbstractIndex, self)._get_backend(using)
-            
+
         self._backend_alias = using
         return self._backend
 
