e69e956a32453f9ab1c34cc13fff21e1b0bdf359
carderm <michael.carder@michaelcarder.co.uk>
2018-11-29 20:41:24 +1100

    Fixes for previous merge.

--- aldryn_search/router.py
+++ aldryn_search/router.py
@@ -5,14 +5,14 @@ from django.utils.translation import get_language
 from haystack import routers
 from haystack.constants import DEFAULT_ALIAS
 
-from .helpers import get_alias_from_language
+from .utils import alias_from_language
 
 
 class LanguageRouter(routers.BaseRouter):
 
     def for_read(self, **hints):
         language = get_language()
-        alias = get_alias_from_language(language)
+        alias = alias_from_language(language)
 
         if alias not in settings.HAYSTACK_CONNECTIONS:
             return DEFAULT_ALIAS
@@ -21,7 +21,7 @@ class LanguageRouter(routers.BaseRouter):
     def for_write(self, **hints):
         language = None
         try:
-            # Use this for Aldryn Events/FAQ etc 
+            # Use this for Aldryn Events/FAQ etc
             #   as 'get_language()' can fail to get the instance language.
             if hints and 'instance' in hints:
                 if hasattr(hints['instance'],'language_code'):
@@ -37,4 +37,3 @@ class LanguageRouter(routers.BaseRouter):
         if alias not in settings.HAYSTACK_CONNECTIONS:
             return DEFAULT_ALIAS
         return alias
-      
